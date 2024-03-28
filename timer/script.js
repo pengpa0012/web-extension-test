4c0151fb1e48b49c19cd51c98e411694e3363d1b
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-28 11:30:05 +0800

    update timer, added overlay

--- timer/script.js
+++ timer/script.js
@@ -1,14 +1,15 @@
 const body = document.querySelector("body")
 const div = document.createElement("div")
+const overlay = document.createElement("div")
 
 let timer
 
 div.classList = "ui"
 div.innerHTML = `
-  <p class="timer-display hidden">0</p>
+  <p class="timer-display hidden"></p>
   <div class="timer-input">
-    <input type="number" placeholder="Hour" class="hour-input">
-    <input type="number" placeholder="Minutes" class="minute-input">
+    <input type="number" placeholder="Minute" class="minute-input" max="60">
+    <input type="number" placeholder="Second" class="second-input" max="60">
   </div>
   <div class="btn-flex">
   <button class="start-btn">start</button>
@@ -16,34 +17,45 @@ div.innerHTML = `
   </div>
   <button class="close-btn"></button>
 `
+overlay.classList = "overlay hidden"
+overlay.innerHTML = `
+  <div class="overlay-content">
+    <h1>Time's Up!</h1>
+    <button class="close-overlay">close</button>
+  <div>
+`
+
 body.appendChild(div)
+body.appendChild(overlay)
 const ui = document.querySelector(".ui")
 const closeBtn = document.querySelector(".close-btn")
 const timerDisplay = document.querySelector(".timer-display")
-const hourInput = document.querySelector(".hour-input")
 const minuteInput = document.querySelector(".minute-input")
+const secondInput = document.querySelector(".second-input")
 const timerInput = document.querySelector(".timer-input")
 const start = document.querySelector(".start-btn")
 const reset = document.querySelector(".reset-btn")
+const closeOverlay = document.querySelector(".close-overlay")
 
 closeBtn.addEventListener("click", () => ui.classList.toggle("active"))
 start.addEventListener("click", () => {
+  if(!minuteInput.value && !secondInput.value) return
   start.classList.add("hidden")
   reset.classList.remove("hidden")
   timerDisplay.classList.remove("hidden")
   timerInput.classList.add("hidden")
-  let totalTime = parseInt(hourInput.value * 60) + parseInt(minuteInput.value * 60)
+  let totalTime = parseInt(minuteInput.value * 60) + parseInt(secondInput.value)
   timer = setInterval(() => {
-    const hours = Math.floor(totalTime / 60);
-    const minutes = totalTime % 60;
+    const minutes = Math.floor(totalTime / 60)
+    const seconds = totalTime % 60
 
-    timerDisplay.innerHTML = hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0');
+    timerDisplay.innerHTML = minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0')
 
     if (totalTime <= 0) {
-      clearInterval(timer);
-      timerDisplay.innerHTML = "Countdown Finished!";
+      clearInterval(timer)
+      overlay.classList.remove("hidden")
     } else {
-      totalTime--;
+      totalTime--
     }
   }, 1000)
 })
@@ -57,7 +69,4 @@ reset.addEventListener("click", () => {
   clearInterval(timer)
 })
 
-// change to hr and min
-// minus the time per tick
-// countdown
-
+closeOverlay.addEventListener("click", () => overlay.classList.add("hidden"))
