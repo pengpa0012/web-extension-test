f02ae2a87e76fd2498e38388adcb1916ac70ab3b
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-31 20:43:05 +0800

    update style, ui

--- theme/script.js
+++ theme/script.js
@@ -4,6 +4,8 @@ const div = document.createElement("div")
 div.classList = "ui"
 div.innerHTML = `
   <h1>Color Picker</h1>
+  <input type="color" class="color-input"/>
+  <input type="text" placeholder="Enter image URL" class="img-input"/>
   <button class="toggle-btn"></button>
 `
 body.appendChild(div)
