18a811216052114d9c076226bbb805f3b7f8ab82
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-01 20:11:19 +0800

    added change BG

--- theme/script.js
+++ theme/script.js
@@ -12,10 +12,26 @@ body.appendChild(div)
 const ui = document.querySelector(".ui")
 const toggleBtn = document.querySelector(".toggle-btn")
 const colorInput = document.querySelector(".color-input")
+const imgInput = document.querySelector(".img-input")
 
 
+
+changeBG(localStorage.getItem("img-color"), localStorage.getItem("img-url"))
 
 colorInput.addEventListener("input", e => {
-  body.style.background = e.target.value
+  localStorage.setItem("img-color", e.target.value)
+  changeBG(e.target.value, localStorage.getItem("img-url"))
 })
+
+imgInput.addEventListener("keydown", e => {
+  if(e.code == "Enter") {
+    localStorage.setItem("img-url", e.target.value)
+    changeBG(localStorage.getItem("img-color"), e.target.value)
+  }
+})
+
 toggleBtn.addEventListener("click", () => ui.classList.toggle("active"))
+
+function changeBG(color, image) {
+  body.style.background = `url(${image}), ${color}`
+}
